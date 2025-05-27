12a70ed86511b322062a27246a828822e3ba09b7
haka-goktug-torun <hakangoktugtorun@gmail.com>
2025-05-27 17:04:27 +0300

    fazlalıkları tek fonk ile  kontrol altına aldım

--- game/main.cpp
+++ game/main.cpp
@@ -6,45 +6,7 @@
 #include "gameengine.h"
 #include "../services/scoreservice.h"
 
-
-int main() {
-    SetConsoleOutputCP(65001);
-    srand(static_cast<unsigned>(time(nullptr)));
-
-    UIManager::promptLanguage();
-    std::string langChoice;
-    std::cin >> langChoice;
-    if (langChoice == "tr") UIManager::lang = Language::TR;
-    else UIManager::lang = Language::EN;
-
-    std::string shipTypeStr;
-    std::shared_ptr<Ship> ship = PlayerInterface::loadOrCreateShip(shipTypeStr);
-    if (!ship) return 0;
-
-    GameEngine engine;
-    int gameResult = engine.start(ship); // 1 = normal bitis, 0 = yakit bitti, -1 = kullanici cikti
-
-    if (gameResult == -1) return 0; // Kullanıcı "h" dedi, skor vs. yok
-
-    if (gameResult == 0) {
-        if (ship->getFuel() > 0 && ship->getHealth() > 0) {
-            if (PlayerInterface::askToSaveGame()) {
-                ScoreService::saveGame(ship, shipTypeStr);
-            }
-
-            int finalScore = ScoreService::calculate(ship);
-            UIManager::promptName();
-            std::string playerName;
-            std::cin >> playerName;
-            ScoreService::saveToFile(playerName, finalScore);
-            UIManager::printCongratulations(finalScore);
-            ScoreService::displayTop5();
-        }
-        return 0;
-    }
-
-    int finalScore = ScoreService::calculate(ship);
-
+void handleEndOfGame(const std::shared_ptr<Ship>& ship, const std::string& shipTypeStr) {
     if (ship->getFuel() > 0 && ship->getHealth() > 0) {
         if (PlayerInterface::askToSaveGame()) {
             ScoreService::saveGame(ship, shipTypeStr);
@@ -52,11 +14,36 @@ int main() {
 
         UIManager::promptName();
         std::string playerName;
-        std::cin >> playerName;
+        std::getline(std::cin, playerName);
+
+        int finalScore = ScoreService::calculate(ship);
         ScoreService::saveToFile(playerName, finalScore);
         UIManager::printCongratulations(finalScore);
         ScoreService::displayTop5();
     }
+}
+
+int main() {
+    SetConsoleOutputCP(65001);
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    UIManager::promptLanguage();
+    std::string langChoice;
+    std::getline(std::cin, langChoice);
+    UIManager::lang = UIManager::parseLanguageInput(langChoice);
+
+    std::string shipTypeStr;
+    std::shared_ptr<Ship> ship = PlayerInterface::loadOrCreateShip(shipTypeStr);
+    if (!ship) return 0;
+
+    GameEngine engine;
+    int gameResult = engine.start(ship); // 1 = normal bitiş, 0 = yakıt bitti, -1 = kullanıcı çıkışı
+
+    if (gameResult == -1) return 0;
+
+    handleEndOfGame(ship, shipTypeStr);
 
     return 0;
 }
+
+
