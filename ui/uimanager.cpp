71eee462ccc509a9ae92237bc72aafe4840958dd
haka-goktug-torun <hakangoktugtorun@gmail.com>
2025-05-28 10:16:40 +0300

    lang yapısı düzenledni

--- ui/uimanager.cpp
+++ ui/uimanager.cpp
@@ -1,6 +1,8 @@
 #include "uimanager.h"
 #include <algorithm>
 
+Language UIManager::lang = Language::EN;
+
 Language UIManager::parseLanguageInput(const std::string& input) {
     std::string lowerInput = input;
     std::transform(lowerInput.begin(), lowerInput.end(), lowerInput.begin(), ::tolower);
